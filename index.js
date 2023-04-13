f896c4b307555bb969963699682eedd777fea2ae
Sushil kumar <isushilchaudhary@gmail.com>
2023-04-13 11:09:41 +0000

    Do you know me game

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
-const { log, Console } = require('console');
-var readlineSync = require('readline-sync');
+var readlineSync = require("readline-sync");
 
-//for loop
-let printNamelen = readlineSync.questionInt("How many times want to print your name? ")
-console.log(printNamelen)
+var score = 0;
 
-for(var i =0;i<=printNamelen;i++){
-console.log("* ".repeat(i))
-  // for(var j= 0;j<=i;j++){
-    
-  // }
-  
+//List of questions
+const questions = [
+  {
+    question: "Where i do live?",
+    answer: "Fatehpur"
+  },
+  {
+    question: "What is my birthday month?",
+    answer: "April"
+  },
+  {
+    question: "What is my last qualification?",
+    answer: "MCA"
+  },
+  {
+    question: "What is name of my last college?",
+    answer: "NIT Kurukshetra"
+  },
+  {
+    question: "Where i do Work?",
+    answer: "Trejhara solution"
+  },
+  {
+    question: "Place Name, Where i visited last time?",
+    answer: "Lansdowne"
+  }
+];
+
+function welcome() {
+  var name = readlineSync.question("What is your name ?")
+  console.log("Welcome " + name + " to DO YOU KNOW sushil")
 }
+
+function play(question, answer) {
+  userAnswer = readlineSync.question(question);
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log("You are right.")
+    score = score + 1;
+  }
+  else {
+    console.log("You are wrong.")
+    console.log("Right answer is ----> " + answer)
+  }
+}
+
+function game() {
+  for (var i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer);
+  }
+}
+
+function showScore() {
+  console.log("YAY! Your Scored: " + score)
+}
+
+welcome();
+game();
+showScore();
